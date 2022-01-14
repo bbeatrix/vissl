24d4fd8b4b68b65e553d0fa61d7a6f97a8fceb30
bbeatrix <bbea.smile@gmail.com>
2022-01-14 13:07:33 +0000

    Add script to run grid knn eval with sign

---
+++ bash_scripts/run_sign_knneval.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for k in {0..30};
+do
+    echo "Processing for param value $k ..."
+
+    main_config="benchmark/nearest_neighbor/eval_resnet_8gpu_in1k_kNN.yaml"
+    params_file="/data/shared/data/vissl_pretrained_models/simclr_1node_imagenet1k_resnet50_ours/model_final_checkpoint_phase999.torch"
+    checkpoint_dir="./outputs/knneval/sign_grid4/sign_with_param_${k}"
+
+    test_only=False
+    num_gpus=2
+
+    CUDA_VISIBLE_DEVICES=6,7 python tools/nearest_neighbor_test.py config=${main_config} config.DISTRIBUTED.NUM_PROC_PER_NODE=${num_gpus} config.TEST_ONLY=${test_only} config.MODEL.WEIGHTS_INIT.PARAMS_FILE=${params_file} config.CHECKPOINT.DIR=${checkpoint_dir} config.MODEL.FEATURE_EVAL_SETTINGS.SIGN_PARAM_VALUE=${k}
+    echo "Done with $k!"
+
+done
