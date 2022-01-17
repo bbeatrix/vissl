12cdfc05eecce465de80d39f2fcb9ea8f468fb34
bbeatrix <bbea.smile@gmail.com>
2022-01-17 19:01:25 +0000

    Update script for knn eval with sign function

--- extra_scripts/bash_scripts/run_sign_knneval.sh
+++ extra_scripts/bash_scripts/run_sign_knneval.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-for k in {0..30};
+for k in {0.0,1.e-12};
 do
     echo "Processing for param value $k ..."
 
     main_config="benchmark/nearest_neighbor/eval_resnet_8gpu_in1k_kNN.yaml"
     params_file="/data/shared/data/vissl_pretrained_models/simclr_1node_imagenet1k_resnet50_ours/model_final_checkpoint_phase999.torch"
-    checkpoint_dir="./outputs/knneval/sign_grid/sign_with_param_${k}"
+    checkpoint_dir="./outputs/knneval/sign_grid_check/sign_with_param_${k}"
 
     use_sign_layer=True
     test_only=False
-    num_gpus=7
+    num_gpus=6
 
-    CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,7 python tools/nearest_neighbor_test.py config=${main_config} config.DISTRIBUTED.NUM_PROC_PER_NODE=${num_gpus} config.TEST_ONLY=${test_only} config.MODEL.WEIGHTS_INIT.PARAMS_FILE=${params_file} config.CHECKPOINT.DIR=${checkpoint_dir} config.MODEL.TRUNK.RESNETS.USE_SIGN_LAYER=${use_sign_layer} config.MODEL.TRUNK.RESNETS.SIGN_PARAM_VALUE=${k}
+    CUDA_VISIBLE_DEVICES=2,3,4,5,6,7 python tools/nearest_neighbor_test.py config=${main_config} config.DISTRIBUTED.NUM_PROC_PER_NODE=${num_gpus} config.TEST_ONLY=${test_only} config.MODEL.WEIGHTS_INIT.PARAMS_FILE=${params_file} config.CHECKPOINT.DIR=${checkpoint_dir} config.MODEL.TRUNK.RESNETS.USE_SIGN_LAYER=${use_sign_layer} config.MODEL.TRUNK.RESNETS.SIGN_PARAM_VALUE=${k}
     echo "Done with $k!"
 
 done
