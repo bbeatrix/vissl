410f7d9e8e39cba119a0c9552d69a30aef805e93
bbeatrix <bbea.smile@gmail.com>
2022-04-13 09:09:01 +0000

    Update script for merging extracted features

--- extra_scripts/save_merged_extracted_feats.py
+++ extra_scripts/save_merged_extracted_feats.py
@@ -1,19 +1,27 @@
+import logging
 import numpy as np
 from vissl.utils.extract_features_utils import ExtractedFeaturesLoader
 from vissl.utils.misc import merge_features
 
+from vissl.utils.logger import setup_logging, shutdown_logging
 
-extracted_feats_path = "/home/bbea/vissl/outputs/extracted_features/simclr_1node_imagenet1k_resnet50_phase999_final_trunkfeats"
-merged_feats_path = "/home/bbea/vissl/outputs/simclr_1node_imagenet1k_resnet50_phase999_embeddings_dataset/"
+setup_logging(__name__)
+logging.info(f"Logging set up")
+
+extracted_feats_path = "/home/bbea/vissl/outputs/extracted_features/simclr_imagenet1k_resnet50_phase999_final_trunk_feats_3view"
+merged_feats_path = "/home/bbea/vissl/outputs/simclr_1node_imagenet1k_resnet50_phase999_3view_embeddings_dataset/"
 
 layer = "res5avg"
-for split in ["train", "test"]: 
+for split in ["train"]:
     merged_features = ExtractedFeaturesLoader.load_features(input_dir=extracted_feats_path, split=split, layer=layer)
 
     print(merged_features.keys())
 
-    np.save(merged_feats_path + split + "_embeddings.npy", merged_features['features'])
+    np.save(merged_feats_path + split + "_3view_embeddings.npy", merged_features['features'])
     print(merged_features['features'].shape)
 
-    np.save(merged_feats_path + split + "_targets.npy", merged_features['targets'])
+    np.save(merged_feats_path + split + "_3view_targets.npy", merged_features['targets'])
     print(merged_features['targets'].shape)
+
+logging.info("All Done!")
+shutdown_logging()
